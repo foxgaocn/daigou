c03b29b0738ffaf4fc244784d942338a42e19afa
Haiyang (harry) Gao <foxgaocn@gmail.com>
2014-10-28 14:43:48 +1100

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Daigou::Application.config.secret_key_base = 'd59b0c7d3f33f8bf2be27a35f27046bc54e457789fcc94144f98f8bb442819fa0a02c3108385596b713bec9c8ee742c92476b7f07cbddb706a66b5cdcbca3faf'
