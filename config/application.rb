ae2200de38274e5ac8a6e27c220375753eeac317
Haiyang (harry) Gao <foxgaocn@gmail.com>
2014-11-03 14:20:04 +1100

    my store path

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+if File.exists?(File.expand_path('../application.yml', __FILE__))
+  config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+  config.merge! config.fetch(Rails.env, {})
+  config.each do |key, value|
+    ENV[key] ||= value.to_s unless value.kind_of? Hash
+  end
+end
 
 module Daigou
   class Application < Rails::Application
