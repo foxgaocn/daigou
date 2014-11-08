59ebd8ebc0eeea4f54fa64f739e9473aadaef324
Haiyang (harry) Gao <foxgaocn@gmail.com>
2014-11-08 11:59:19 +1100

    symbol link public/system so image will be there

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,8 @@ set :deploy_to, '/home/deploy/daigou'
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
+set :linked_dirs, %w{public/system}
+
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
